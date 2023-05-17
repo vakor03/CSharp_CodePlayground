25ef8f35d4141c0fd485429d447a728d7d9d3ddb
valerii.korniienko <vakor.dev@gmail.com>
2023-05-17 16:05:48 +0300

    Basic gradient descent algorithm created

---
+++ GradientDescent.Algorithm/GradientDescent.cs
@@ -0,0 +1,39 @@
+﻿namespace GradientDescent.Algorithm;
+
+public class GradientDescent
+{
+    public double[] PerformGradientDescent(double[] modelParameters, double[][] trainingData, double[] targets,
+        double learningRate, int maxIterations)
+    {
+        int numFeatures = modelParameters.Length;
+        int numExamples = trainingData.Length;
+
+        for (int iteration = 0; iteration < maxIterations; iteration++)
+        {
+            for (int i = 0; i < numExamples; i++)
+            {
+                double[] features = trainingData[i];
+                double prediction = ComputePrediction(modelParameters, features);
+                double error = prediction - targets[i];
+
+                for (int j = 0; j < numFeatures; j++)
+                {
+                    modelParameters[j] -= learningRate * error * features[j];
+                }
+            }
+        }
+
+        return modelParameters;
+    }
+
+    private double ComputePrediction(double[] modelParameters, double[] features)
+    {
+        double prediction = 0.0;
+        for (int i = 0; i < modelParameters.Length; i++)
+        {
+            prediction += modelParameters[i] * features[i];
+        }
+
+        return prediction;
+    }
+}
---
+++ GradientDescent.Algorithm/GradientDescentParallel.cs
@@ -0,0 +1,37 @@
+﻿namespace GradientDescent.Algorithm;
+
+public class GradientDescentParallel
+{
+    public double[] PerformParallelGradientDescent(double[] modelParameters, double[][] trainingData, double[] targets, double learningRate, int maxIterations)
+    {
+        int numFeatures = modelParameters.Length;
+        int numExamples = trainingData.Length;
+
+        for (int iteration = 0; iteration < maxIterations; iteration++)
+        {
+            Parallel.For((long)0, numExamples, i =>
+            {
+                double[] features = trainingData[i];
+                double prediction = ComputePrediction(modelParameters, features);
+                double error = prediction - targets[i];
+
+                for (int j = 0; j < numFeatures; j++)
+                {
+                    Interlocked.Add(ref modelParameters[j]), -learningRate * error * features[j]);
+                }
+            });
+        }
+
+        return modelParameters;
+    }
+
+    private double ComputePrediction(double[] modelParameters, double[] features)
+    {
+        double prediction = 0.0;
+        for (int i = 0; i < modelParameters.Length; i++)
+        {
+            prediction += modelParameters[i] * features[i];
+        }
+        return prediction;
+    }
+}
