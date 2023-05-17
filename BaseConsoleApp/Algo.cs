b56a2217821ed68afe0c723f0ba899b94aa00fd1
valerii.korniienko <vakor.dev@gmail.com>
2023-05-17 14:55:19 +0300

    Basic solution + basic ConsoleApp

---
+++ BaseConsoleApp/Algo.cs
@@ -0,0 +1,22 @@
+﻿namespace BaseConsoleApp;
+
+public class Algo
+{
+    public static int Sum(int a, int b)
+    {
+        List<int> randomList = new List<int>();
+        for (int i = 0; i < 1000; i++)
+        {
+            randomList.Add(i);
+        }
+        foreach (var i in randomList)
+        {
+            foreach (var i1 in randomList)
+            {
+                var c = i + i1;
+            }
+        }
+
+        return 2;
+    } 
+}
---
+++ BaseConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BaseConsoleApp;
+
+Console.WriteLine("Hello, World!");
+int[][] array2D = new int[1000][];
+Algo algo = new Algo();
+Algo.Sum(1,1);
